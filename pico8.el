dd184efd3b4dc5ea21db94d0128ed1e209c0b9a0
Taylor Skidmore <taylor@taylorskidmore.com>
2018-02-21 16:25:48 -0500

    fix some typos

--- pico8.el
+++ pico8.el
@@ -1,10 +1,10 @@
 ;; pico-8.el -- major mode for editing pico-8 cartridges -*- coding: utf-8; lexical-binding: t; -*-
 
 (setq pico8-font-lock-keywords
-      (let* ((pico-constants '("true" "fasle" "nil"))
+      (let* ((pico-constants '("true" "false" "nil"))
              (pico-keywords '("for" "do" "end" "function" "foreach" "if" "then"))
-             (pico-functions-graphics '("clip" "pget" "pset" "sget" "sset" "fget" "fset" "print" "cursor" "color" "cls" "camera" "circ" "circfill" "line" "rect" "rectfill" "pal" "palt" "spr" "sspr" "fillp"))
-             (pico-functions-tables '("add" "del" "all" "foreach" "paris"))
+             (pico-functions-graphics '("clip" "pget" "pset" "sget" "sset" "fget" "fset" "cursor" "color" "cls" "camera" "circ" "circfill" "line" "rect" "rectfill" "pal" "palt" "spr" "sspr" "fillp"))
+             (pico-functions-tables '("add" "del" "all" "foreach" "pairs"))
              (pico-functions-misc '("btn" "btnp" "sfx" "music" "mget" "mset" "map"))
              (pico-functions-memory '("peek" "poke" "memcpy" "reload" "cstore" "memset"))
              (pico-functions-math '("max" "min" "mid" "flr" "cos" "sin" "atan2" "sqrt" "abs" "rnd" "srand" "band" "bor" "bxor" "bnot" "shl" "shr"))
@@ -26,6 +26,7 @@
           (,pico-keywords-regexp . font-lock-keyword-face))))
 
 (define-derived-mode pico8-mode lua-mode "pico-8 mode"
+(define-derived-mode pico8-mode lua-mode "pico-8"
   (setq font-lock-defaults '((pico8-font-lock-keywords))))
 
 ;;;###autoload
